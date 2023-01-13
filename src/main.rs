aeef9ed4e3a723cebcc82b486721fd10a6491264
Ben Woodcroft <benjwoodcroft@gmail.com>
2023-01-14 06:59:31 +1000

    Draft EaselSequence.

--- src/main.rs
+++ src/main.rs
@@ -436,3 +436,41 @@ impl Drop for HmmsearchResult {
         }
     }
 }
+
+struct EaselSequence {
+    c_sq: *mut libhmmer_sys::ESL_SQ,
+}
+
+impl EaselSequence {
+    fn new(alphabet: *const libhmmer_sys::ESL_ALPHABET) -> Self {
+        let c_sq = unsafe { libhmmer_sys::esl_sq_CreateDigital(alphabet) };
+        Self { c_sq }
+    }
+
+    /// Replace (or initialise) the sequence data in this object with the given
+    /// sequence. This method is a simplification of the sqascii_ReadSequence()
+    /// C function in easel.
+    /// 
+    /// The seq given here should remain valid for the lifetime of this object,
+    /// or until this method is called again, because the internal C pointer
+    /// will be incorrect, causing undefined behavior.
+    fn replace_sequence(&mut self, seq: &[u8]) {
+        let n = seq.len() as i64;
+
+        unsafe {
+            // addbuf(sqfp, sq, n);
+            (*self.c_sq).seq = seq.as_ptr() as *mut i8;
+
+            // sq->start = 1;
+            (*self.c_sq).start = 1;
+            // sq->end   = sq->n;
+            (*self.c_sq).end = n;
+            // sq->C     = 0;
+            (*self.c_sq).C = 0;
+            // sq->W     = sq->n;
+            (*self.c_sq).W = n;
+            // sq->L     = sq->n;
+            (*self.c_sq).L = n;
+        }
+    }
+}
